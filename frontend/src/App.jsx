58872d2d144aba627fc30cfaa7643de1cd2e1638
Mohd Abdul Mannan <ma24042000@gmail.com>
2024-04-23 07:42:01 +0530

    setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <p className="font-bold text-center p-5 bg-blue-500">Setup</p>
+    </>
+  )
+}
+
+export default App
